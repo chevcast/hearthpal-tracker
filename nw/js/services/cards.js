bb4f9b456e4f74907330197125ce6c8fe9f764f8
Alex Ford <alex.ford@codetunnel.com>
2015-09-03 14:07:19 -0600

    Removed accidentally checked in nw directory.

--- nw/js/services/cards.js
+++
@@ -1,8 +0,0 @@
-angular.module('app').factory('cards', function ($q, socket) {
-  return new $q(function (resolve, reject) {
-    socket.perform('getCards', null, function (err, cards) {
-      if (err) { return reject(err); }
-      resolve(cards); 
-    });
-  });
-});
